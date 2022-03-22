25e17aa717352a27cabb3b3cb7ea00c8d10bdc7c
JoelCreatesGames <62071362+JoelCreatesGames@users.noreply.github.com>
2022-03-21 21:40:51 -0300

    fix multiline fennel breaking

--- UnityProject/Assets/UnityLuaFennelStarter/Scripts/Extensions/LuaExtensions.cs
+++ UnityProject/Assets/UnityLuaFennelStarter/Scripts/Extensions/LuaExtensions.cs
@@ -5,7 +5,9 @@ public static class LuaExtensions
     public static void DoFnlString(this Lua lua, string chunk, string saveTo = null)
     {
         string prefix = string.IsNullOrEmpty(saveTo) ? "" : saveTo + " = ";
-        chunk = chunk.Replace("\"", "\\\"");
+        chunk = chunk.Replace("\"", "\\\"")
+                     .Replace(System.Environment.NewLine, "");
+        UnityEngine.Debug.Log("here is what we are going to eval:\n" + prefix + "fennel.eval(\"" + chunk + "\")");
         lua.DoString(prefix + "fennel.eval(\"" + chunk + "\")");
     }
 }
